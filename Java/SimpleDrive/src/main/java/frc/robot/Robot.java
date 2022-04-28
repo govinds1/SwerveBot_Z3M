f8b84af28bb8515ac3b1a316495c629dd0a17c9c
Govind Suresh <govindsuresh2001@gmail.com>
2022-04-28 01:36:35 -0400

    Add controller control to TeleopPeriodic

--- Java/SimpleDrive/src/main/java/frc/robot/Robot.java
+++ Java/SimpleDrive/src/main/java/frc/robot/Robot.java
@@ -5,6 +5,7 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
@@ -19,6 +20,9 @@ public class Robot extends TimedRobot {
   private static final String kCustomAuto = "My Auto";
   private String m_autoSelected;
   private final SendableChooser<String> m_chooser = new SendableChooser<>();
+
+  private SwerveDrive m_swerveDrive = new SwerveDrive();
+  private XboxController m_xboxController = new XboxController(0);
 
   /**
    * This function is run when the robot is first started up and should be used for any
@@ -78,7 +82,13 @@ public class Robot extends TimedRobot {
 
   /** This function is called periodically during operator control. */
   @Override
-  public void teleopPeriodic() {}
+  public void teleopPeriodic() {
+    double forward = -m_xboxController.getLeftY();
+    double right = m_xboxController.getLeftX();
+    double rot = m_xboxController.getRightX();
+
+    m_swerveDrive.Drive(right, forward, rot);
+  }
 
   /** This function is called once when the robot is disabled. */
   @Override
